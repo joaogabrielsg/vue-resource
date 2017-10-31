6e5051a1a59e3aff5bf8065495d20537b12f4bec
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2017-10-31 09:34:28 -0300

    change request method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-      <h1>HTTP</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>HTTP</h1>
+
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username">
+        </div>
+
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" v-model="user.email">
+        </div>
+
+        <button class="btn btn-primary" @click="submit">Submit</button>
+
+        <button class="btn btn-primary" @click="fetchData">Get Data</button>
+
+        <hr>
+
+        <ul class="list-group">
+          <li class="list-group-item" v-for="user in users">{{ user.username }} / {{ user.email }}</li>
+        </ul>
+
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        email: ''
+      },
+      users: []
+    };
+  },
+  methods: {
+    submit() {
+      this.$http.post('', this.user).then(response => {
+        console.log(response);
+      }, error => {
+        console.log(error);
+      });
+    },
+
+    fetchData() {
+      this.$http.get('').then(response => {
+        return response.json();
+      }).then(data => {
+        const resultArray = [];
+        for (let key in data) {
+          resultArray.push(data[key]);
+          console.log(data[key]);
+        }
+        console.log(data);
+        this.users = resultArray;
+      });
+    }
+  }
 }
 </script>
 
 <style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://vuejs-http-d09eb.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if(request.method == 'POST'){
+    request.method = 'PUT';
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
